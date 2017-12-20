f6ac18c8f4c95d20616d118d120458211ebbfc9e
mick.yi <mick.yi@embracesource.com>
2017-12-20 16:18:37 +0800

    增加特征索引转换

--- src/main/scala/com/es/preprocess/FeatureIndex.scala
+++ src/main/scala/com/es/preprocess/FeatureIndex.scala
@@ -1,26 +1,24 @@
 package com.es.preprocess
 
 import com.es.util.DataFrameUtil
-import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.types.{LongType, StructField}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.types.{StructType, StructField, LongType};
-
 import scopt.OptionParser
 
 /**
   * Created by mick.yi on 2017/12/20.
-  * 普通的tfidf
+  * 特征索引
   */
-object WordTFIDF {
-
+object FeatureIndex {
   /** 命令行参数 */
   case class Params(input: String = "", //输入数据,parquet格式
-                    idfPath: String = "", //词的逆文档词频保存路径
+                    featurePath: String = "", //特征保存路径
                     output: String = "", //输出数据,parquet格式
-                    inputCol: String = "", //分词后文本列
+                    inputCol: String = "", //特征文本列
                     outputCol: String = "", //结果输出列
                     resultCols: String = "", //输出结果保留的列,默认全部输出
-                    appName: String = "WordTFIDF"
+                    appName: String = "FeatureIndex"
                    )
 
   def main(args: Array[String]) {
@@ -30,16 +28,16 @@ object WordTFIDF {
     }
 
     val default_params = Params()
-    val parser = new OptionParser[Params]("WordTFIDF") {
-      head("WordTFIDF:.")
+    val parser = new OptionParser[Params]("FeatureIndex") {
+      head("FeatureIndex:.")
       opt[String]("input")
         .required()
         .text("输入数据")
         .action((x, c) => c.copy(input = x))
-      opt[String]("idfPath")
+      opt[String]("featurePath")
         .required()
-        .text("词的逆文档词频保存路径")
-        .action((x, c) => c.copy(idfPath = x))
+        .text("特征保存路径")
+        .action((x, c) => c.copy(featurePath = x))
       opt[String]("output")
         .required()
         .text("输出数据")
@@ -76,10 +74,8 @@ object WordTFIDF {
 
     import sqlContext.implicits._
     val inputDF = sqlContext.read.parquet(p.input)
-    //加载idf
-    val idfDF = sc.textFile(p.idfPath).
-      map(_.split(" ")).
-      map(arr => (arr(0), arr(1).toDouble)).toDF("word", "idf")
+    //加载特征文本
+    val featureIdxMap = sc.textFile(p.featurePath).distinct().collect().zipWithIndex.toMap
 
     //增加索引列id
     val schema = inputDF.schema.add(StructField("id", LongType, true))
@@ -89,42 +85,24 @@ object WordTFIDF {
     }
     val newDF = sqlContext.createDataFrame(newRdd, schema)
 
-    //列转行
-    val dataRDD = newDF.select("id", p.inputCol).map(row => {
+    //特征名转特征索引
+    val dataDF = newDF.select("id", p.inputCol).map(row => {
       val rowIdx = row.getAs[Long]("id") //行号
       val text = row.getAs[String](p.inputCol)
-      text.split(" ").zipWithIndex.map { case (word, colIdx) => {
-        (word, colIdx, rowIdx) //词，列索引，行索引
+      val newText=text.split(" ").map { feature => {
+        val arr=feature.split(":")
+        val featureName=arr(0)
+        val feafureVal=arr(1).toDouble
+        val featureIndex=featureIdxMap.getOrElse(featureName,-1)
+        featureIndex+":"+feafureVal //从 特征名:特征值 转为 特征索引号:特征值
+      }.mkString(" ")
       }
-      }
-    }).flatMap(x => x)
+      (rowIdx,newText)
+    }).toDF("id",p.outputCol)
 
-    //计算词频TF
-    val tfDF = dataRDD.map { case (word, colIdx, rowIdx) => (word, 1) }.
-      rdd.
-      reduceByKey(_ + _).
-      toDF("word", "tf")
-
-    //计算tfidf
-    val wordTfidfDF = dataRDD.toDF("word", "colIdx", "id").
-      join(idfDF, "word").
-      join(tfDF, "word").rdd.map(row => {
-      val tf = row.getAs[String]("tf").toDouble
-      val idf = row.getAs[Double]("idf")
-      val word = row.getAs[String]("word")
-      val colIdx = row.getAs[Int]("colIdx")
-      val rowIdx = row.getAs[Long]("id")
-      val tfidf = tf * idf //tfidf值
-      (rowIdx, List.apply((word + ":" + tfidf, colIdx)))
-    }).reduceByKey(_ ::: _).
-      map { case (id, list) => {
-        val str = list.sortBy(_._2).map(_._1).mkString(" ") //保持原有的列顺序，空格合并所有的word:tfidf
-        (id, str)
-      }
-      }.toDF("id", p.outputCol)
 
     //转换数据
-    val outputDF = wordTfidfDF.join(newDF, "id")
+    val outputDF = dataDF.join(newDF, "id")
 
     //保存结果
     val resultDF = DataFrameUtil.select(outputDF, p.resultCols) //只保存选择的列
@@ -132,6 +110,5 @@ object WordTFIDF {
 
     sc.stop()
   }
-
 
 }
